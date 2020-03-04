d4c779348a1b956a23cba24f8b7191b2af6fed36
I400156 <d.schoenmakers@student.fontys.nl>
2020-03-04 09:44:29 +0100

    added id with menu

--- src/main/java/com/foodplanner/rest_service/databasemodel/Menu.java
+++ src/main/java/com/foodplanner/rest_service/databasemodel/Menu.java
@@ -1,9 +1,23 @@
 package com.foodplanner.rest_service.databasemodel;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "menu", schema = "DeVoedselbank")
 public class Menu {
+    private int id;
+
+    @Id
+    @Column(name = "id")
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
 }
