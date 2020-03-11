30e7e075838d9abc93185633ec013405cbd8c29b
Wesley <wesley.verhagen@student.fontys.nl>
2020-03-11 09:51:27 +0100

    Removed obsolete import

--- src/main/java/com/foodplanner/rest_service/controller/MenuController.java
+++ src/main/java/com/foodplanner/rest_service/controller/MenuController.java
@@ -1,6 +1,6 @@
 package com.foodplanner.rest_service.controller;
 
-import jdk.jshell.spi.ExecutionControl;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
