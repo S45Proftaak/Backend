05add4123076162f2e1c1e4d15d823ef9ef1e236
Wesley <wesley.verhagen@student.fontys.nl>
2020-04-15 14:29:41 +0200

    Added FoodOrdersPerWeek Endpoint

--- src/main/java/com/foodplanner/rest_service/controller/FoodOrderController.java
+++ src/main/java/com/foodplanner/rest_service/controller/FoodOrderController.java
@@ -2,6 +2,7 @@ package com.foodplanner.rest_service.controller;
 
 import com.foodplanner.rest_service.databasemodel.FoodOrder;
 import com.foodplanner.rest_service.databasemodel.User;
+import com.foodplanner.rest_service.dtos.WeekDTO;
 import com.foodplanner.rest_service.logic.jwt.JwtTokenProvider;
 import com.foodplanner.rest_service.repositories.FoodOrderRepository;
 import com.foodplanner.rest_service.repositories.UserRepository;
@@ -38,16 +39,33 @@ public class FoodOrderController {
         return foodOrderRepository.findAllByUser(user);
     }
 
+    @GetMapping(value = "getOrdersPerWeek")
+    @ResponseBody
+    public List<Date> getFoodOrdersPerWeek(HttpServletRequest req, @RequestBody WeekDTO weekDTO){
+        String token = tokenProvider.resolveToken(req);
+
+        List<Date> foundDates = new ArrayList<>();
+
+        for (FoodOrder order : foodOrderRepository.findAllByUser(userRepository.findById(tokenProvider.getUserIdFromToken(token)).get())){
+            for (Date date : weekDTO.getDates()){
+                if(date.equals(order.getDate())){
+                    foundDates.add(date);
+                }
+            }
+        }
+        return foundDates;
+    }
+
     @PostMapping(value = "addNewFoodOrder")
     @ResponseBody
-    public void addNewFoodOrder (HttpServletRequest req){
+    public void addNewFoodOrder (HttpServletRequest req, @RequestParam(value = "date") Date date){
 
         String token = tokenProvider.resolveToken(req);
 
         User user = userRepository.findById(tokenProvider.getUserIdFromToken(token)).get();
         FoodOrder newOrder = new FoodOrder();
         newOrder.setUser(user);
-        newOrder.setDate(java.sql.Date.valueOf(java.time.LocalDate.now()));
+        newOrder.setDate(date);
         foodOrderRepository.save(newOrder);
     }
 }
