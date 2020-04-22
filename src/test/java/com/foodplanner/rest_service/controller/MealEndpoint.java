fc1ab51c2eee4cd936b5e5f3cdff46aeef89c243
unknown <ricomuijtjens@ziggo.nl>
2020-04-22 12:54:45 +0200

    Can' t run test

--- src/test/java/com/foodplanner/rest_service/controller/MealEndpoint.java
+++ src/test/java/com/foodplanner/rest_service/controller/MealEndpoint.java
@@ -2,42 +2,43 @@ package com.foodplanner.rest_service.controller;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import com.foodplanner.rest_service.databasemodel.FoodOrder;
-import com.foodplanner.rest_service.dtos.LoginDTO;
 import com.foodplanner.rest_service.ldap.PersonRepository;
+import com.foodplanner.rest_service.logic.jwt.JwtTokenProvider;
 import com.foodplanner.rest_service.repositories.FoodOrderRepository;
+import com.foodplanner.rest_service.repositories.UserRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.ldap.core.LdapTemplate;
 import org.springframework.mock.web.MockHttpServletRequest;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
-import javax.naming.ldap.LdapName;
+import javax.servlet.http.HttpServletRequest;
 import java.sql.Date;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.Silent.class)
-@ActiveProfiles("test")
 public class MealEndpoint {
 
     @InjectMocks
     FoodOrderController foodOrderController;
 
     @InjectMocks
-    UserController userController;
+    JwtTokenProvider provider;
 
     @Mock
     FoodOrderRepository foodOrderRepository;
 
     @Mock
-    PersonRepository repository;
+    UserRepository userRepository;
+
+
 
     @Test
     public void getFoodOrder_Failed() {
@@ -57,20 +58,19 @@ public class MealEndpoint {
 
     @Test
     public void getFoodOrder_Accepted() {
-        ResponseEntity<?> loginEntity = userController.loginUser(new LoginDTO("testemail2@gmail.com", "secret"));
-
-
+        String bearerToken = provider.createToken(2, "Jihn Die", "Employee");
         MockHttpServletRequest request = new MockHttpServletRequest();
-        request.addHeader("Authorization:", loginEntity.getBody());
+        request.addHeader("Authorization", "Bearer " + bearerToken);
         RequestContextHolder.setRequestAttributes(new ServletRequestAttributes(request));
 
         String str = "2015-03-31";
         Date date = Date.valueOf(str);
 
         when(foodOrderRepository.save(any(FoodOrder.class))).thenReturn(new FoodOrder());
+        when(provider.resolveToken(any(HttpServletRequest.class))).thenReturn(bearerToken);
 
         ResponseEntity<Object> responseEntity = foodOrderController.addNewFoodOrder(request, date);
 
-        assertThat(responseEntity.getStatusCodeValue()).isEqualTo(400);
+        assertThat(responseEntity.getStatusCodeValue()).isEqualTo(200);
     }
 }
