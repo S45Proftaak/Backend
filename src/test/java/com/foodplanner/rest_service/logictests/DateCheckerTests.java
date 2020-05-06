3e45275e1d0f6d55442efc229194231abeec1e8f
Duncan Schoenmakers <400156@student.fontys.nl>
2020-05-06 16:39:00 +0200

    small fix with Gregorian Calendar that has a glitch with using numbers in the month section, changed to Calendar class

--- src/test/java/com/foodplanner/rest_service/logictests/DateCheckerTests.java
+++ src/test/java/com/foodplanner/rest_service/logictests/DateCheckerTests.java
@@ -3,6 +3,7 @@ package com.foodplanner.rest_service.logictests;
 import com.foodplanner.rest_service.logic.foodorder.DateChecker;
 import org.junit.jupiter.api.Test;
 
+import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
 
@@ -16,14 +17,14 @@ public class DateCheckerTests {
     public void dateToLate()
     {
         dateChecker = new DateChecker();
-        Date tolate = new GregorianCalendar(2018, 2, 11).getTime();
+        Date tolate = new GregorianCalendar(2018, Calendar.FEBRUARY, 11).getTime();
         assertTrue(dateChecker.areYouToLate(tolate));
     }
     @Test
     public void dateNotToLate()
     {
         dateChecker = new DateChecker();
-        Date tolate = new GregorianCalendar(2022, 2, 11).getTime();
+        Date tolate = new GregorianCalendar(2022, Calendar.FEBRUARY, 11).getTime();
         assertFalse(dateChecker.areYouToLate(tolate));
     }
 
