e51c113fbdecf7089abf30aa54ec3bcd2916dc2e
unknown <ricomuijtjens@ziggo.nl>
2020-05-06 14:31:32 +0200

    AddPoints change

--- src/main/java/com/foodplanner/rest_service/logic/scoreboard/PointDivider.java
+++ src/main/java/com/foodplanner/rest_service/logic/scoreboard/PointDivider.java
@@ -1,15 +1,16 @@
 package com.foodplanner.rest_service.logic.scoreboard;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class PointDivider {
     private static final Long IN_TIME = 10L;
     private static final Long TOO_LATE = 4L;
 
-    public Long addPointsAndReturn(boolean tooLate, Long currPoints){
+    public void addPoints(boolean tooLate, Long currPoints){
         if(tooLate){
             currPoints += TOO_LATE;
         }else{
             currPoints += IN_TIME;
         }
-        return currPoints;
     }
 }
