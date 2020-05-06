1f005e58fb527110b2d7e44a18659f8a270212cc
unknown <ricomuijtjens@ziggo.nl>
2020-05-06 13:30:25 +0200

    PointDivider

--- src/main/java/com/foodplanner/rest_service/endpoints/ScoreBoardEndpoint.java
+++ src/main/java/com/foodplanner/rest_service/endpoints/ScoreBoardEndpoint.java
@@ -1,5 +1,5 @@
 package com.foodplanner.rest_service.endpoints;
 
 public interface ScoreBoardEndpoint {
-    public String GET_SCOREBOARD = "get-scoreboard";
+    public String GET_SCOREBOARD = "/get-scoreboard";
 }
--- src/main/java/com/foodplanner/rest_service/logic/scoreboard/PointDivider.java
+++ src/main/java/com/foodplanner/rest_service/logic/scoreboard/PointDivider.java
@@ -1,10 +1,15 @@
 package com.foodplanner.rest_service.logic.scoreboard;
 
 public class PointDivider {
-    private static Long IN_TIME = 10L;
-    private static Long TOO_LATE = 4L;
+    private static final Long IN_TIME = 10L;
+    private static final Long TOO_LATE = 4L;
 
-    private boolean checkTime(){
-
+    public Long addPointsAndReturn(boolean tooLate, Long currPoints){
+        if(tooLate){
+            currPoints += TOO_LATE;
+        }else{
+            currPoints += IN_TIME;
+        }
+        return currPoints;
     }
 }
