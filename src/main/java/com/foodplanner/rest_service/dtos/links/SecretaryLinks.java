971e3960a06a71e0a5c454732dc17e6f02bc28db
unknown <ricomuijtjens@ziggo.nl>
2020-05-12 15:00:14 +0200

    Link mistake

--- src/main/java/com/foodplanner/rest_service/dtos/links/SecretaryLinks.java
+++ src/main/java/com/foodplanner/rest_service/dtos/links/SecretaryLinks.java
@@ -5,5 +5,5 @@ import com.foodplanner.rest_service.endpoints.SecretaryEndpoint;
 public class SecretaryLinks {
     public String BASE = SecretaryEndpoint.BASE;
     public String GET_USERS_BY_DATE = SecretaryEndpoint.GET_USERS_BY_DATE;
-    public String GET_USERS_BETWEEN_DATES = SecretaryEndpoint.GET_USERS_BY_DATE;
+    public String GET_USERS_BETWEEN_DATES = SecretaryEndpoint.GET_USERS_BETWEEN_DATES;
 }
