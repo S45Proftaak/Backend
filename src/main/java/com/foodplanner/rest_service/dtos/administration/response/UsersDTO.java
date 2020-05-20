b3f38be187af0996bb6427be8755ec62b7b96c80
Bon Moolenburgh <40125958+bontib@users.noreply.github.com>
2020-05-20 11:13:49 +0200

    user roles kunnen geweizigd worden

---
+++ src/main/java/com/foodplanner/rest_service/dtos/administration/response/UsersDTO.java
@@ -0,0 +1,27 @@
+package com.foodplanner.rest_service.dtos.administration.response;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.foodplanner.rest_service.databasemodel.User;
+
+import java.util.List;
+
+public class UsersDTO {
+
+    @JsonAlias("Users")
+    private List<User> users;
+
+    public UsersDTO(List<User> users) {
+        this.users = users;
+    }
+
+    public UsersDTO() {
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+}
--- src/main/java/com/foodplanner/rest_service/endpoints/AdministrationEndpoints.java
+++ src/main/java/com/foodplanner/rest_service/endpoints/AdministrationEndpoints.java
@@ -3,4 +3,7 @@ package com.foodplanner.rest_service.endpoints;
 public interface AdministrationEndpoints {
     String BASE = "/administration";
     String UPDATE_PRICE = "/updatePrice";
+    String GET_ALL_USERS = "/getAllUsers";
+    String UPDATE_USER_ROLE = "/updateUserRole";
+    String GET_ROLES = "/getRoles";
 }
