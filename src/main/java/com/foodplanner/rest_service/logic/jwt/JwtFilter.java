88b44bf83fcf6bbe81b17b87b7e14734d8f1f8db
unknown <ricomuijtjens@ziggo.nl>
2020-05-26 14:32:43 +0200

    Connection to external LDAP Server

--- src/main/java/com/foodplanner/rest_service/logic/jwt/JwtFilter.java
+++ src/main/java/com/foodplanner/rest_service/logic/jwt/JwtFilter.java
@@ -29,7 +29,7 @@ public class JwtFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
         String token = jwtTokenProvider.resolveToken(httpServletRequest);
         String path = (httpServletRequest).getRequestURI();
-        if(!path.startsWith("/auth/login")){
+        if(!path.startsWith("/auth/")){
             try{
                 jwtTokenProvider.validateToken(token);
                 String email = jwtTokenProvider.getEmailFromToken(token);
